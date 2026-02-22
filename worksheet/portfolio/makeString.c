a53a2d9daf48c46467e9d69681142790ec778e4d
MemonM01 <vmdk0165@leeds.ac.uk>
2026-02-22 10:27:32 +0000

    worksheet complete

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Muhammad Shafay Memon
+Student ID: 201957435
 */
 
 #include <stdio.h>
@@ -10,10 +10,18 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char holder[100];
+    
 
     // process the command-line data using appropriate string functions
+    for (int i = 1; i < argc; i++) {
+        strcat(holder, argv[i]);
+        if (i < argc - 1) {
+            strcat(holder, "-");
+        }
+    }
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    printf("%s\n", holder); // use only this single print statement in your submitted code
 
     return 0;
 }
